d3107bc5a8863fcf7c6da4e4486cf7a0f0ad985f
Chris <user@Users-MacBook-Pro-6.local>
2015-12-13 16:07:51 -0500

    Initial Commit

---
+++ atm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  atm
+//
+//  Created by Chris on 12/13/15.
+//  Copyright (c) 2015 Chris. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
